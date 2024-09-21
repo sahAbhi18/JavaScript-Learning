5916895d74d6abcf31ef35afaf79f2980d953d8d
Abhishek Kumar Sah <sahabhishekm@gmail.com>
2024-09-21 11:45:18 +0000

    conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+/*let score = "33abc"
+console.log(typeof(score));
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+/*
+33 => number
+"33abc" => NaN(Not a number)
+true =>1; false=>0
+*/
+
+let isLoggedIn = "22abc";
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+/*
+1=> true;0=> false
+"22abc"=> true
+*/
+
+let head = 45
+let stringhead = String(head)
+console.log(typeof(stringhead));
+
