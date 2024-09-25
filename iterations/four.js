07f81d962fe871ec36e026b71fb6bf31da0d4446
Abhishek Kumar Sah <sahabhishekm@gmail.com>
2024-09-25 13:37:49 +0000

    forin loop

---
+++ iterations/four.js
@@ -0,0 +1,17 @@
+let myObj = {
+    js: "javascript",
+    cpp: "C++",
+    rb: "Ruby",
+    swift: "swift by apple"
+}
+for (const key in myObj) {
+    //console.log(`The value of ${key} object is: ${myObj[key]}`);
+    
+}
+
+let programming = ["js","cpp","ruby","swift"]
+for (const key in programming) {
+    console.log(programming[key]);
+    
+    
+}
