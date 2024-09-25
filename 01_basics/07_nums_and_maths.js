87ddbf451fbda7ae52b61ef69b5bbd94d2100b92
Abhishek Kumar Sah <sahabhishekm@gmail.com>
2024-09-25 12:15:37 +0000

    NUMBER

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,13 @@
+let score = 300
+//console.log(score);
+
+let otherNumber = new Number(700)
+//console.log(otherNumber.toString());
+//console.log(otherNumber.toFixed(2));
+ let hundreds = 20000000
+ //console.log(hundreds.toLocaleString("en-IN"));
+ let balance = 244.877
+ //console.log(balance.toPrecision(3));
+ 
+/* **********************************MATH************************************************ */
+
