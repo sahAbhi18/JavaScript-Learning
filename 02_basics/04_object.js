7931bd77e66a04472ffb5a84a821b85710979ec4
Abhishek Kumar Sah <sahabhishekm@gmail.com>
2024-10-01 06:52:50 +0000

    nested object

---
+++ 02_basics/04_object.js
@@ -0,0 +1,58 @@
+let tinderUser = new Object()      //singleton Object.
+// let tinderUser = {}    // Non-singleton object.
+
+tinderUser.id = "123abc"
+tinderUser.email = "some@gmail.com"
+tinderUser.name = "bca_abhi"
+tinderUser.isLogged = false 
+//console.log(tinderUser);
+//console.log(tinderUser.name);
+//nested Objects
+
+let regularUser = {
+    email: "unknown@gmail.com",
+    Gender: "Male",
+    fullName: {
+        userName: "unkown_abhi",
+        userFullName: {
+            firstName: "Abhishek",
+            lastName: "Sah",
+        }
+    }
+}
+//console.log(regularUser);
+//console.log(regularUser.fullName.userFullName.lastName);
+// How to combine two objects?
+
+let obj1 = {
+    firstName: "Rohan",
+    lastName: "Kumar"
+}
+let obj2 = {
+    email: "rohan@123@gmail.com",
+    Gende: "Male"
+}
+//let obj3 = Object.assign({},obj1,obj2)
+let obj3 = {...obj1,...obj2}
+//console.log(obj3);
+
+
+
+const user = [
+{
+    id: 1,
+    email: "abhi@gmail.com",
+
+},
+  {
+    id:2,
+    email: "ritik@gmail.com",
+
+},
+ {
+    id: 13,
+    email: "ashu@gmail.com",
+
+}];
+console.log(user[1].email);
+
