e097f3fbe4c1cf9d6ec4cc4ac31e18dd988a7498
Abhishek Kumar Sah <sahabhishekm@gmail.com>
2024-10-01 14:15:17 +0000

    Function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,20 @@
+function myName(){
+    console.log("A");
+    console.log("b");
+    console.log("h");
+    console.log("i");
+    console.log("s");
+    console.log("h");
+    console.log("e");
+    console.log("k"); 
+}
+
+myName()
+/* [IMPORTANT POINT] 
+Parameter() => when you denfine in the () its called parameter.
+Arguments() => when you excute in the () its called arguments.
+*/
+
+function addTwoNumber(){
+    
+}
