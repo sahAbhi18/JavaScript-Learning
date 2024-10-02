535e574a5b60b5c3e0958160145c36b9908627cb
Abhishek Kumar Sah <sahabhishekm@gmail.com>
2024-10-02 10:09:57 +0000

    function in depth

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -1,12 +1,12 @@
 function myName(){
-    console.log("A");
-    console.log("b");
-    console.log("h");
-    console.log("i");
-    console.log("s");
-    console.log("h");
-    console.log("e");
-    console.log("k"); 
+    //console.log("A");
+    //console.log("b");
+    //console.log("h");
+    //console.log("i");
+    //console.log("s");
+    //console.log("h");
+    //console.log("e");
+    //console.log("k"); 
 }
 
 myName()
@@ -15,6 +15,39 @@ Parameter() => when you denfine in the () its called parameter.
 Arguments() => when you excute in the () its called arguments.
 */
 
-function addTwoNumber(){
-    
+function addTwoNumber(x,y){
+    //console.log(x+y);
+    return x+y;
 }
+const result = addTwoNumber(7,5)
+//console.log(result);
+function loginUserMessage(username){
+    if(!username ){
+        console.log(`please enter the username`);
+        
+    }
+    return `${username} just logged in`
+}
+const loggedIn = loginUserMessage("Abhishek")
+//console.log(loggedIn);
+
+function shoppingCart (...num1){
+    return num1;
+}
+//console.log(shoppingCart(200,300,500,700));
+
+let user = {
+    username: "Abhi_123",
+    Gender: "Male"
+}
+ function handleObject (anyobject){
+    return `user is ${anyobject.username} and gender is  ${anyobject.Gender}`
+ }
+ //console.log(handleObject(user));
+ 
+ let myArr = [100,200,300,400,500]
+ function handlearray (getArray){
+    return getArray
+ }
+ console.log(handlearray(myArr));
+ 
