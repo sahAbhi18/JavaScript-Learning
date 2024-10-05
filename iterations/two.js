4d1d63863fee5a0073eb22778ee2219ba92beeeb
Abhishek Kumar Sah <sahabhishekm@gmail.com>
2024-10-05 06:39:08 +0000

    while & do while loop

---
+++ iterations/two.js
@@ -0,0 +1,17 @@
+let i=0;
+while (i<=10) {
+    //console.log(`The value is ${i}`);
+    i++   
+}
+let myarr = ["flash","shaktiman","Batman","Superman"]
+let arr =0;
+while (arr<myarr.length) {
+    //console.log(`The value is ${myarr[arr]}`);
+    arr++;  
+}
+
+let score = 1
+do{
+    console.log(`the value is ${score}`)
+    score++;
+} while(score<=10);
