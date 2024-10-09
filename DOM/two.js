b384da74f8e3eeca2377ec31ca77ad9312344fe7
Abhishek Kumar Sah <sahabhishekm@gmail.com>
2024-10-09 05:36:50 +0000

    DOM completed

---
+++ DOM/two.js
@@ -0,0 +1,22 @@
+let parent = document.querySelector('.parent')
+//console.log(parent);
+//console.log(parent.children);
+//console.log(parent.children[1].innerHTML);
+for (let i = 0; i<parent.children.length; i++) {
+    //console.log(parent.children[i].innerHTML);
+    
+}
+parent.children[1].style.color='pink'
+parent.children[2].style.color='skyblue'
+parent.children[3].style.color='red'
+//console.log(parent.lastElementChild);
+//console.log(parent.firstElementChild);
+ let dayOne = document.querySelector('.day')
+ //console.log(dayOne);
+ //console.log(dayOne.parentElement);
+ //console.log(dayOne.nextElementSibling);
+ console.log("NODES", parent.childNodes);
+ 
+ 
+ 
+
